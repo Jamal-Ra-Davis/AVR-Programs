2ee6ff7cfce25eda857b5292ced6cdb636bb57e7
Jamal Davis <Jamal.Ra.Davis@gmail.com>
2017-08-21 19:36:15 -0700

    Think improved speed of calculating interrupt timing

--- PovTest/pov_test.c
+++ PovTest/pov_test.c
@@ -1,22 +1,40 @@
 #include <avr/io.h>
 #include <util/delay.h>
 #include <avr/interrupt.h>
+#include <avr/pgmspace.h>
+
+#define F_CPU 1000000UL
 
 #define FBUF_SZ 60
+#define FBUF_HEIGHT 3
 #define LED_PORT PORTB
 #define LED_DDR	DDRB
 #define TIMER_MAX 249
 
+#define CLK_PIN (1 << PB5)
+#define LAT_PIN (1 << PB2)
+#define DATA_PIN (1 << PB3)
+
 #define DEFAULT_INT_DELAY 68
+
+typedef struct
+{
+    uint8_t buffer[FBUF_HEIGHT][FBUF_SZ];
+	int buf_idx;
+} FrameBuffer;
+
 
 int buf_idx;
 uint8_t fbuf[FBUF_SZ];
 
-uint64_t SYS_TIMER;
-uint64_t time_delta;
+uint32_t SYS_TIMER;
+uint32_t time_delta;
 
 
-uint8_t NUM_BUF[10][8] = {
+FrameBuffer frame_buf;
+
+
+const uint8_t NUM_BUF[10][8] PROGMEM = {
 							{0x00, 0x00, 0x3C, 0x42, 0x42, 0x3C, 0x00, 0x00},//0
 							{0x00, 0x00, 0x00, 0x22, 0x7E, 0x02, 0x00, 0x00},//1
 							{0x00, 0x00, 0x26, 0x4A, 0xA2, 0x22, 0x00, 0x00},//2
@@ -42,48 +60,76 @@ uint8_t NUM_BUF[10][8] = {
 							{0x00, 0x04, 0x04, 0x1C, 0x24, 0x24, 0x18, 0x00} //9
 						};
 						*/
-void writeDigit(uint8_t digit, int index, uint8_t *buf)
-{
-	if (digit >= 10)
-		return;
-	for (int i=0; i<8; i++)
-	{
-		int write_idx = index + i;
-		if (write_idx >= FBUF_SZ)
-			write_idx -= FBUF_SZ;
+void writeDigit(uint8_t digit, int index, int z, FrameBuffer *buf);
+void writeNumber(long number, int idx, int z, FrameBuffer *buf);
+uint8_t calcIntDelay(uint16_t period);
+void setPixel(int x, int y, int z, FrameBuffer *buf);
+void clearPixel(int x, int y, int z, FrameBuffer *buf);
+void clearBuffer(FrameBuffer *buf);
+void setBuffer(FrameBuffer *buf);
 
-		buf[write_idx] |= NAME_BUF[digit][i];
-	}
-}
-uint8_t calcIntDelay(int period)//Period in increments of 250us
+uint8_t spi_tranceiver(uint8_t byte)
 {
-	//period in uS
-	//float out_float = (period/(8*FBUF_SZ)) - 1;
-	float out_float = ((period*25)/(48)) - 1; //Simplified for easier math
-	int out = (int)(out_float + 0.5);
-
-	if (out > 255)
-		return 255;
-	if (out <= 5)
-		return 5;
-	return out;
+    SPDR = byte;
+    while (!(SPSR & (1 << SPIF)));
+    return SPDR;
 }
 
-ISR(INT0_vect)
+
+ISR(INT0_vect)//Hall-effect trigger
 {
-	time_delta = SYS_TIMER - time_delta;
-	if (time_delta >= 150)
+	uint32_t delta = SYS_TIMER - time_delta;
+	time_delta = SYS_TIMER;
+	if (delta >= 1000)
 		OCR0A = DEFAULT_INT_DELAY;
+	else if (delta >= 491)
+		OCR0A = 255;
+	else if (delta <= 32)
+		OCR0A = 16;
 	else
-		OCR0A = calcIntDelay(time_delta);
+	{
+		//OCR0A = calcIntDelay(delta);
+
+		uint32_t val = 533*delta - 512;//== 533*delta + 512 - 1024
+		/*
+		if (val % 1024U < 512)
+		{
+			val = val >> 10;
+			val--;
+		}
+		else
+		{
+			val = val >> 10;
+		}
+		*/
+		OCR0A = (uint8_t)(val >> 10);
+	}
+
 	TCNT0 = 0;
-	//LED_PORT ^= 0xFF;
 	buf_idx = 0;
-	LED_PORT = fbuf[buf_idx];
+	//LED_PORT = fbuf[buf_idx];
 }
+int w_idx, b_idx;
 ISR(TIMER0_COMPA_vect)
 {
-	LED_PORT = fbuf[buf_idx];
+	//LED_PORT = fbuf[buf_idx];
+
+	w_idx = buf_idx + 20;
+	if (w_idx >= FBUF_SZ)
+		w_idx -= FBUF_SZ;
+
+	b_idx = buf_idx + 40;
+	if (b_idx >= FBUF_SZ)
+		b_idx -= FBUF_SZ;
+
+
+	LED_PORT &= ~LAT_PIN;
+	//spi_tranceiver(fbuf[buf_idx]);
+	spi_tranceiver(frame_buf.buffer[2][b_idx]);
+	spi_tranceiver(frame_buf.buffer[1][w_idx]);
+	spi_tranceiver(frame_buf.buffer[0][buf_idx]);
+	LED_PORT |= LAT_PIN;
+
 	buf_idx++;
 	if (buf_idx >= FBUF_SZ)
 		buf_idx = 0;	
@@ -92,28 +138,8 @@ ISR(TIMER2_COMPA_vect)
 {
 	SYS_TIMER++;
 }
-void setPixel(int x, int y, uint8_t *buf)
-{
-	if (x < 0 || x >= FBUF_SZ || y < 0 || y >= 8)
-		return;
-	buf[x] |= (1 << y);
-}
-void clearPixel(int x, int y, uint8_t *buf)
-{
-    if (x < 0 || x >= FBUF_SZ || y < 0 || y >= 8)
-        return;
-    buf[x] &= ~(1 << y);
-}
-void clearBuffer(uint8_t *buf)
-{
-	for (int i=0; i<FBUF_SZ; i++)
-		buf[i] = 0x00;
-}
-void setBuffer(uint8_t *buf)
-{
-    for (int i=0; i<FBUF_SZ; i++)
-        buf[i] = 0xFF;
-}
+
+
 int main(void) 
 {
     //-----------INITS------------//
@@ -122,11 +148,49 @@ int main(void)
 	int i=0;
 	uint8_t toggle = 0;
 	SYS_TIMER = 0;
-
+	uint32_t delta;
+	
 
 	buf_idx = 0;
 	for (int j=0; j<FBUF_SZ; j++)
 		fbuf[j] = 0;
+
+	
+	frame_buf.buf_idx = 0;	
+	for (int j=0; j<FBUF_SZ; j++)
+	{
+		frame_buf.buffer[0][j] = 0;
+		frame_buf.buffer[1][j] = 0;
+		frame_buf.buffer[2][j] = 0;
+	}
+	for (int j=0; j<FBUF_SZ; j++)
+	{
+		if (j < 25)
+		{
+			frame_buf.buffer[0][j] = 0x05;
+			frame_buf.buffer[1][j] = 0x0A;
+			frame_buf.buffer[2][j] = 0xF0;
+
+			if (j != 0 && j%4 == 0)
+			{
+				uint8_t mask = 0xA0;
+				frame_buf.buffer[1][j] |= mask;
+				frame_buf.buffer[2][j] &= ~mask;
+			}
+		}
+		else
+		{
+			frame_buf.buffer[0][j] = 0x55;
+            frame_buf.buffer[1][j] = 0xAA;
+		}
+	} 
+	for (int j=50; j<FBUF_SZ; j++)
+	{
+		frame_buf.buffer[0][j] = 0;
+        frame_buf.buffer[1][j] = 0;
+        frame_buf.buffer[2][j] = 0;	
+	}	
+
 
 	//Enable CTC mode with div by 8
 	TCCR0A |= (1 << WGM01); //CTC
@@ -149,10 +213,74 @@ int main(void)
 	EICRA |= (1 << ISC01);
 	EIMSK |= (1 << INT0);
 
+
+	SPCR |= (1 << SPE) | (1 << MSTR);
+	PORTB &= ~(CLK_PIN | DATA_PIN | LAT_PIN);
 	sei();
-	time_delta = SYS_TIMER - 0;
+	
+//	while(1)//Freedom loop
+//	{
+		_delay_ms(1000);
+//	}
+
+	
+	//Shift register test
+/*
+	uint8_t sr_val[3] = {0x81, 0xF0, 0xAA};
+	
+
+	while(1)
+	{
+		PORTB &= ~LAT_PIN;
+		for (int i=0; i<3; i++)
+		{
+			spi_tranceiver(sr_val[2-i]);
+		}	
+        PORTB |= LAT_PIN;
+        _delay_ms(500);
+
+		PORTB &= ~LAT_PIN;
+		spi_tranceiver(0);
+		spi_tranceiver(0);
+		spi_tranceiver(0);
+        PORTB |= LAT_PIN;
+        _delay_ms(500);
+	}
+
+	while (1)
+	{
+		PORTB &= ~LAT_PIN;
+		for (int i=0; i<3; i++)
+		{
+			for (int j=0; j<8; j++)
+			{
+				PORTB &= ~CLK_PIN;
+				if (sr_val[0] & (1 << (7-j)))
+					PORTB |= DATA_PIN;
+				else
+					PORTB &= ~DATA_PIN;
+				PORTB |= CLK_PIN;
+			}
+			//sr_val[3-i] = ~sr_val[3-i];
+			sr_val[0] = ~sr_val[0];
+		}	
+		PORTB |= LAT_PIN;
+		_delay_ms(500);
+	}
+*/
 
 
+	//Write time delta after "1000ms" delay
+	delta = SYS_TIMER - 0;
+	clearBuffer(&frame_buf);
+	for (int i=0; i<FBUF_SZ; i++)
+		frame_buf.buffer[0][i] |= 0x01;	
+	writeNumber(delta, -5, 0, &frame_buf);
+    writeNumber(delta, -5, 1, &frame_buf);
+	writeNumber(delta, -5, 2, &frame_buf);
+    _delay_ms(5000);
+	i=0;
+	delta = SYS_TIMER;
     //-------EVENT LOOP-----------//
     while(1) 
     {
@@ -173,6 +301,36 @@ int main(void)
 		//_delay_ms(100);
 		continue;
 		*/
+/*
+		//Test that numerical display works
+		for (int i=0; i<256; i++)
+		{
+			clearBuffer(&frame_buf);
+			writeNumber(i, 15, 2, &frame_buf);
+			_delay_ms(250);
+		}
+		//writeNumber(735, 15, fbuf);
+		//_delay_ms(1000);
+		continue;
+		//write out time delta from interrupt
+		//time_delta = SYS_TIMER - time_delta;
+*/		
+
+		delta = SYS_TIMER-delta;
+		clearBuffer(&frame_buf);
+		writeNumber(delta, 30, 2, &frame_buf);
+		writeNumber(i, 15, 1, &frame_buf);
+		i++;
+		delta = SYS_TIMER;
+		while((SYS_TIMER-delta) < 2000)
+		{
+			_delay_us(50);
+		}
+		//time_delta = SYS_TIMER;
+		//_delay_ms(500);
+		continue;
+
+		//Do snazzy rotating animation with 0 position marker
 		for (int j=0; j<8; j++)
 		{
 			for (i=0; i<FBUF_SZ; i++)
@@ -190,6 +348,7 @@ int main(void)
 		}
 		continue;
 
+		//Draw triangle wave basically
 		for (i=0; i<8; i++)
 		{
 			LED_PORT = (1 << i);
@@ -220,3 +379,93 @@ int main(void)
     return(0);
 
 }
+
+
+
+void writeDigit(uint8_t digit, int index, int z, FrameBuffer *buf)
+{
+	if (z >= FBUF_HEIGHT || z < 0)
+		return;
+    if (digit >= 10)
+        return;
+    for (int i=0; i<8; i++)
+    {
+        //int write_idx = index + i;
+		int write_idx = index - i;
+
+        if (write_idx >= FBUF_SZ)
+            write_idx -= FBUF_SZ;
+		if (write_idx < 0)
+			write_idx += FBUF_SZ;
+
+      //buf[write_idx] |= NUM_BUF[digit][i];
+		//buf[write_idx] |= NUM_BUF[digit][7-i];
+        buf->buffer[z][write_idx] |= pgm_read_byte(&(NUM_BUF[digit][7-i]));
+
+    }
+}
+void writeNumber(long number, int idx, int z, FrameBuffer *buf)
+{
+	if (z >= FBUF_HEIGHT || z < 0)
+        return;
+    if (idx >= FBUF_SZ)
+        idx -= FBUF_SZ;
+	if (idx < 0)
+        idx += FBUF_SZ;
+    while(1)
+    {
+        writeDigit(number%10, idx, z, buf);
+        number /= 10;
+        if (number == 0)
+            break;
+        //idx += 8;
+		idx -= 8;
+        if (idx >= FBUF_SZ)
+            idx -= FBUF_SZ;
+		if (idx < 0)
+			idx += FBUF_SZ;
+    }
+}
+uint8_t calcIntDelay(uint16_t period)//Period in increments of 250us
+{
+    //period in uS
+    //float out_float = (period/(8*FBUF_SZ)) - 1;
+    float out_float = ((period*25)/(48)) - 1; //Simplified for easier math
+    int out = (int)(out_float + 0.5);
+
+    if (out > 255)
+        return 255;
+    if (out <= 5)
+        return 5;
+    return out;
+}
+void setPixel(int x, int y, int z, FrameBuffer *buf)
+{
+    if (x < 0 || x >= FBUF_SZ || y < 0 || y >= 8 || z >= FBUF_HEIGHT || z < 0)
+        return;
+    buf->buffer[z][x] |= (1 << y);
+}
+void clearPixel(int x, int y, int z, FrameBuffer *buf)
+{
+    if (x < 0 || x >= FBUF_SZ || y < 0 || y >= 8 || z >= FBUF_HEIGHT || z < 0)
+        return;
+    buf->buffer[z][x] &= ~(1 << y);
+}
+void clearBuffer(FrameBuffer *buf)
+{
+	for (int k=0; k<FBUF_HEIGHT; k++)
+	{
+    	for (int i=0; i<FBUF_SZ; i++)
+        	buf->buffer[k][i] = 0x00;
+	}
+}
+void setBuffer(FrameBuffer *buf)
+{
+	for (int k=0; k<FBUF_HEIGHT; k++)
+	{
+    	for (int i=0; i<FBUF_SZ; i++)
+        	buf->buffer[k][i] = 0xFF;
+	}
+}
+
+
